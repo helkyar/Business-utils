76dffa9e56150b6bceb9bf4f566baaebeb102aa7
helkyar <89964202+helkyar@users.noreply.github.com>
2021-09-19 21:23:22 +0200

    Add image save

--- DrawApp/app.js
+++ DrawApp/app.js
@@ -10,12 +10,17 @@
 window.addEventListener('load', () => {
   const reset = document.querySelector('.reset');
   const save = document.querySelector('.save');
+  const img = document.querySelector('img');
   const canvas = document.querySelector('.canvas');
   const context = canvas.getContext('2d');
 
   //   Resizing
-  canvas.height = window.innerHeight - 100;
-  canvas.width = window.innerWidth - 20;
+  canvas.height = 0;
+  canvas.width = 0;
+
+  function canvasDefault() {
+    canvas.classList.toggle('hidden');
+  }
 
   let painting = false;
 
@@ -45,8 +50,25 @@ window.addEventListener('load', () => {
   canvas.addEventListener('mouseup', end);
   canvas.addEventListener('mousemove', draw);
 
+  canvas.addEventListener('touchstart', start);
+  canvas.addEventListener('touchend', end);
+  canvas.addEventListener('touchmove', draw);
+
+  img.addEventListener('click', () => {
+    canvas.classList.remove('hidden');
+    img.alt = '';
+    canvas.height = window.innerHeight - 100;
+    canvas.width = window.innerWidth - 20;
+  });
+
   // Buttons =======================================
   reset.onclick = () => {
     location.reload();
+  };
+  save.onclick = () => {
+    img.src = canvas.toDataURL();
+    canvasDefault();
+    canvas.height = 0;
+    canvas.width = 0;
   };
 });
